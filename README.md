01aef71c628327db362f775ab3460a41fb6f90cb
Jayakrishna Alwar <alwar.jayakrishna11011@gmail.com>
2019-03-16 11:42:13 +0530

    Initial Commit
    Changes will follow

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # open-source
+
+[How to create a sharable python package ?](https://towardsdatascience.com/build-your-first-open-source-python-project-53471c9942a7)
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+
+
+with open("README.md", "r") as readme_file:
+
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+
+
+setup(
+
+    name="open-source",
+
+    version="0.0.2",
+
+    author="Jayakrishna Alwar",
+
+    author_email="alwar.jayakrishna11011@gmail.com",
+
+    description="A package to opensource",
+
+    long_description=readme,
+
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/open-source/homepage/",
+
+    packages=find_packages(),
+
+    install_requires=requirements,
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3.7",
+
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+
+    ],
+
+)
